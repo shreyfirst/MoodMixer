305c3b8fd876073c495cea852f504118231e9fe7
shreyfirst <shreysgupta@gmail.com>
2018-01-22 17:33:59 -0800

    fixed crash after accept error

--- app/screens/CameraScreen.js
+++ app/screens/CameraScreen.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+'use strict';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+import Camera from 'react-native-camera';
 
-export default class App extends React.Component {
+  
+export default class CameraScreen extends Component {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>CAM up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam; }}
+          style={styles.preview}
+          type="back"
+          aspect={Camera.constants.Aspect.fill}>
+          <Text style={styles.capture} onPress={() => {}}>[CAPTURE]</Text>
+        </Camera>
       </View>
     );
   }
@@ -16,8 +32,19 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
   },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
 });
--- app/screens/Login.js
+++ app/screens/Login.js
@@ -13,26 +13,34 @@ import Spotify from 'react-native-spotify';
 let spotifyOptions = {
   "clientID": "69708bf2b35e4cc2b208fafa600ce9a9",
   "sessionUserDefaultsKey": "SpotifySession",
-  "redirectURL": "MoodMixer://auth",
+  "redirectURL": "https://github.com/shreyfirst/moodmixer",
   "scopes": ["user-read-private", "playlist-read", "playlist-read-private", "streaming"],
 };
 console.log("Spotify",Spotify)
 export default class App extends React.Component {
 
   render() {
-
+      const { navigate } = this.props.navigation;
       Spotify.initialize(spotifyOptions, (loggedIn, error) => {
           if (error != null) {
               Alert.alert("ERROR", error.message);
           }
       });
-  
+
+      Spotify.login((loggedIn, error) => {
+        if(error)
+        {
+          Alert.alert("Error", error.message);
+        }
+        if(loggedIn)
+        {
+          navigate('CameraScreen')
+        }
+  });
 
     return (
       <View style={styles.container}>
-        <Text>Login up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Please wait as we login you in :)</Text>
       </View>
     );
   }
